b0209d675fca6fdd8524c1fc4943fabfd36b5f76
dreadrat <65046523+dreadrat@users.noreply.github.com>
2024-02-01 12:02:17 +1100

    added scoring sounds

--- lib/game/cubit/gameplay/scoring_cubit.dart
+++ lib/game/cubit/gameplay/scoring_cubit.dart
@@ -1,4 +1,6 @@
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pducky/game/entities/ball/behaviors/bouncing_behaviour.dart';
 
 enum BallImage {
   Puppy,
@@ -15,8 +17,13 @@ class ScoringCubit extends Cubit<ScoringState> {
     score: 0,
     streak: 0,
     wrongTaps: 0,
+    direction: MovementDirection.Left,
   );
 
+
+  void updateDirection(MovementDirection direction) {
+    emit(state.copyWith(direction: direction));
+  }
   void updateBallImage(BallImage image) {
     emit(state.copyWith(ballImage: image));
     print('updateBallImage: $image'); // Add this
@@ -27,17 +34,26 @@ class ScoringCubit extends Cubit<ScoringState> {
    
   }
 
-  void increaseScore() {
+  void increaseScore(MovementDirection direction) {
     emit(state.copyWith(
       score: state.score + 1
     ));
-    print('Scored'); 
+
+    switch (direction) {
+      case MovementDirection.Left:
+        FlameAudio.play('score_left.mp3');
+        break;
+      case MovementDirection.Right:
+        FlameAudio.play('score_right.mp3');
+        break;
+    }
   }
 
   void increaseStreak() {
     emit(state.copyWith(
       streak: state.streak + 1
     ));
+    
   }
 
    void increaseWrongTaps() {
@@ -48,9 +64,10 @@ class ScoringCubit extends Cubit<ScoringState> {
   void resetStreak() {
     emit(state.copyWith(streak: 0));
   }
+
   bool checkForScore(BallImage buttonImage) {
     if(state.ballImage == buttonImage && state.ballIsInScoringZone) {
-      increaseScore();
+      increaseScore(state.direction); // Use the current direction here
       increaseStreak();
       return true;
     } else if (state.ballImage != buttonImage && state.ballIsInScoringZone) {
@@ -59,7 +76,6 @@ class ScoringCubit extends Cubit<ScoringState> {
     }
     return false;
   }
-
 }
 
 class ScoringState {
@@ -69,6 +85,7 @@ class ScoringState {
   final int score;
   final int streak;
     final int wrongTaps; 
+    final MovementDirection direction;
 
   const ScoringState({
     required this.ballImage,
@@ -76,6 +93,7 @@ class ScoringState {
     required this.score,
     required this.streak,
      required this.wrongTaps, 
+     required this.direction,
   });
 
   ScoringState copyWith({
@@ -84,7 +102,8 @@ class ScoringState {
     int? score, 
     int? streak,
     int? wrongTaps,
-  }) {
+    MovementDirection? direction, 
+     }) {
 
      return ScoringState(
     ballImage: ballImage ?? this.ballImage, 
@@ -92,6 +111,8 @@ class ScoringState {
     score: score ?? this.score,
     streak: streak ?? this.streak,
     wrongTaps: wrongTaps ?? this.wrongTaps,
+    direction: direction ?? this.direction, // And this
+
   );
   
   }
