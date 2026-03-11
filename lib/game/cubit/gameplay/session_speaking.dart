37c214026552939a2bfb19d1038b905e4df9b1fa
Will <will@local>
2026-03-11 20:25:29 +1100

    Fix skip-intro: guard all intro speech components

--- lib/game/cubit/gameplay/session_speaking.dart
+++ lib/game/cubit/gameplay/session_speaking.dart
@@ -36,61 +36,61 @@ class SessionSpeaking {
     bool skipIntro = false,
   }) {
     sessionCubit.timedSpeechComponents = [
-      if (!skipIntro)
-
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename: 'hi,_and_welcome_to_puppyduck',
+      if (!skipIntro) ...[
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename: 'hi,_and_welcome_to_puppyduck',
+          ),
+          startTime: 3,
         ),
-        startTime: 3,
-      ),
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename:
-              'today,_we_are_going_to_be_working_on_that_negative_thought_you_just_can\'t_seem_to_shake',
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename:
+                'today,_we_are_going_to_be_working_on_that_negative_thought_you_just_can\'t_seem_to_shake',
+          ),
+          startTime: 7,
         ),
-        startTime: 7,
-      ),
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename:
-              'psychologists_call_this_rumination,_and_it_has_been_linked_to_negative_mental_health_outcomes',
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename:
+                'psychologists_call_this_rumination,_and_it_has_been_linked_to_negative_mental_health_outcomes',
+          ),
+          startTime: 15,
         ),
-        startTime: 15,
-      ),
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename:
-              'I_want_you_to_focus_on_the_ball_going_side_to_side._When_its_in_the_scoring_zone,_tap_the_matching_puppy...or_duck..._to_score',
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename:
+                'I_want_you_to_focus_on_the_ball_going_side_to_side._When_its_in_the_scoring_zone,_tap_the_matching_puppy...or_duck..._to_score',
+          ),
+          startTime: 22,
         ),
-        startTime: 22,
-      ),
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename:
-              'don\'t_worry_too_much_about_the_score_though,_this_is_really_about_engaging_bilateral_stimulation',
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename:
+                'don\'t_worry_too_much_about_the_score_though,_this_is_really_about_engaging_bilateral_stimulation',
+          ),
+          startTime: 35,
         ),
-        startTime: 35,
-      ),
-      TimedSpeechComponent(
-        speechComponent: SpeechComponent(
-          ball: game.puppyDuck,
-          sessionCubit: sessionCubit,
-          filename:
-              'for_best_results,_pop_on_some_headphones._Studies_have_shown_bilateral_stimulation_can_be_more_effective_if_visual,_tactile_and_auditory_stimulation_is_combined',
+        TimedSpeechComponent(
+          speechComponent: SpeechComponent(
+            ball: game.puppyDuck,
+            sessionCubit: sessionCubit,
+            filename:
+                'for_best_results,_pop_on_some_headphones._Studies_have_shown_bilateral_stimulation_can_be_more_effective_if_visual,_tactile_and_auditory_stimulation_is_combined',
+          ),
+          startTime: 45,
         ),
-        startTime: 45,
-      ),
+      ],
     ];
 
     // 10-minute guided script.
